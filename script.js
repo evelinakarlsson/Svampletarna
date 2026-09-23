019d1215955387bb2e6bb34f1e13b016b5d6ac50
evelina.karlsson <evelina.karlsson91@outlook.com>
2026-09-23 11:29:28 +0200

    Småfix CSS och HTML. Lagt till kommentarer

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-
+// Deklsrerar konstanter för rätt namn och rätt lösenord vid inloggning
 const rattNamn = "Kalle";
 const rattLosenord = "qwe123";
 
+// Skapar konstanter med referenser till HTML-element för att kuna läsa av eller ändra dem i javascript
 const namnInput = document.getElementById("namn");
 const losenordInput = document.getElementById("losenord");
 const msgElem = document.getElementById("message");
 const loginForm = document.getElementById("loginForm");
+const loginBtn = document.getElementById("btn1");
 const logoutBtn = document.getElementById("btn2");
 
+// Gömmer logga ut-knappen eftersom ingen är inloggad när sidan laddas
 logoutBtn.style.display = "none";
 
-document.getElementById("btn1").addEventListener("click", loggaIn);
-document.getElementById("btn2").addEventListener("click", loggaUt);
+// Kopplar funktioner till knapparna, så att något händer när man klickar på dem. När logga in-knappen klickas körs
+// funktionen loggaIn och när logga ut-knappen klickas körs loggaUt
+loginBtn.addEventListener("click", loggaIn);
+logoutBtn.addEventListener("click", loggaUt);
 
+// Funktion som körs när man klickar på logga in-knappen
 function loggaIn() {
+    // Först kollar funktionen om det man skrivit i fälten stämmer med både rätt namn och lösenord
+    // om det stämmer visas ett välkomstmeddelande
     if (namnInput.value === rattNamn && losenordInput.value === rattLosenord) {
-        msgElem.innerHTML = "Välkommen " + namnInput.value + ", du är nu inloggad";
+        msgElem.textContent = "Välkommen " + namnInput.value + ", du är nu inloggad";
+        msgElem.style.color = ""; // Återställer textfärgen om man testat att logga in och uppgivit felaktiga uppgifter,
+                                  // för då blir texten röd, och behöver därför nollställas för att välkomstexten inte också ska bli röd
 
+        // När man är inloggad göms inloggningsformuläret och logga ut-knappen visas
         loginForm.style.display = "none";
         logoutBtn.style.display = "inline-block";
     } else {
-        msgElem.innerHTML = "Felaktiga inloggningsuppgifter";
+        // Om namn eller lösenord är fel så visas ett felmeddelande i röd text
+        msgElem.textContent = "Felaktiga inloggningsuppgifter";
+        msgElem.style.color = "red";
     }
 }
 
+// Denna funktion körs när man klickar på logga ut-knappen
 function loggaUt() {
-    loginForm.style.display = "grid"; 
-    logoutBtn.style.display = "none";
-    msgElem.innerHTML = "";
+    loginForm.style.display = "grid"; // Visar inloggningsformuläret igen
+    logoutBtn.style.display = "none"; // Logga ut-knappen döljs
+
+    // Rensar bort eventuella tidigare meddelanden samt tömmer inmatningsfälten
+    msgElem.textContent = "";
     namnInput.value = "";
     losenordInput.value = "";
 }
